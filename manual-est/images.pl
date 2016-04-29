2cd94b67c52dd01a8f9589e28481e91b19bb8d65
eead-csic-compbio <compbio@eead.csic.es>
2016-04-29 09:52:48 +0200

    Updated math images

--- manual-est/images.pl
+++ manual-est/images.pl
@@ -2,39 +2,39 @@
 # Associate images original text with physical files.
 
 
-$key = q/<95%;MSF=1.6/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="50" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img2.png"
- ALT="$ &lt;95\%$">|; 
-
 $key = q/>75%;MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="50" HEIGHT="30" ALIGN="MIDDLE" BORDER="0"
+ WIDTH="49" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img3.png"
  ALT="$ &gt;75\%$">|; 
 
 $key = q/G;MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="16" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ WIDTH="16" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img4.png"
  ALT="$ G$">|; 
 
+$key = q/<95%;MSF=1.6/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="49" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$ &lt;95\%$">|; 
+
 $key = q/G^{2};MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="23" HEIGHT="37" ALIGN="MIDDLE" BORDER="0"
+ WIDTH="22" HEIGHT="35" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img5.png"
  ALT="$ G^{2}$">|; 
 
 $key = q/geq40;MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="36" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ WIDTH="36" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img1.png"
  ALT="$ \geq 40$">|; 
 
 $key = q/.slashinstall.pl;MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="78" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ WIDTH="77" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img6.png"
  ALT="$ ./install.pl$">|; 
 
--- manual/images.pl
+++ manual/images.pl
@@ -4,21 +4,21 @@
 
 $key = q/G;MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="17" HEIGHT="14" ALIGN="BOTTOM" BORDER="0"
+ WIDTH="16" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img1.png"
- ALT="$G$">|; 
+ ALT="$ G$">|; 
 
 $key = q/G^{2};MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="24" HEIGHT="16" ALIGN="BOTTOM" BORDER="0"
+ WIDTH="22" HEIGHT="35" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img2.png"
- ALT="$G^{2}$">|; 
+ ALT="$ G^{2}$">|; 
 
 $key = q/Evalue<max(Evalue);MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="171" HEIGHT="32" ALIGN="MIDDLE" BORDER="0"
+ WIDTH="155" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img3.png"
- ALT="$Evalue &lt; max(Evalue)$">|; 
+ ALT="$ Evalue &lt; max(Evalue)$">|; 
 
 1;
 
