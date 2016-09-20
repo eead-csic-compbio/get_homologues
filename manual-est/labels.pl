eb47ee1bc0523a77abb30b4d9e9da7e4c8eafeaa
eead-csic-compbio <compbio@eead.csic.es>
2016-09-20 16:20:47 +0200

    Added pfam_enrich.pl example

--- manual-est/labels.pl
+++ manual-est/labels.pl
@@ -2,27 +2,7 @@
 # Associate labels original text with physical files.
 
 
-$key = q/fig:pant2/;
-$external_labels{$key} = "$URL/" . q|manual-est.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:annotcluster/;
-$external_labels{$key} = "$URL/" . q|manual-est.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:ESTcaveats/;
-$external_labels{$key} = "$URL/" . q|manual-est.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/transcripts2cds/;
-$external_labels{$key} = "$URL/" . q|manual-est.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/perlmods/;
-$external_labels{$key} = "$URL/" . q|manual-est.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:snapshot/;
+$key = q/fig:hclustpange/;
 $external_labels{$key} = "$URL/" . q|manual-est.html|; 
 $noresave{$key} = "$nosave";
 
@@ -30,19 +10,7 @@ $key = q/fig:RAMtimev2/;
 $external_labels{$key} = "$URL/" . q|manual-est.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/default/;
-$external_labels{$key} = "$URL/" . q|manual-est.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:ANImat/;
-$external_labels{$key} = "$URL/" . q|manual-est.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:CNS/;
-$external_labels{$key} = "$URL/" . q|manual-est.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:histid/;
+$key = q/install/;
 $external_labels{$key} = "$URL/" . q|manual-est.html|; 
 $noresave{$key} = "$nosave";
 
@@ -50,23 +18,11 @@ $key = q/tab:algs/;
 $external_labels{$key} = "$URL/" . q|manual-est.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig:pangenomet/;
-$external_labels{$key} = "$URL/" . q|manual-est.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/tab:ANIsample/;
-$external_labels{$key} = "$URL/" . q|manual-est.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/install/;
-$external_labels{$key} = "$URL/" . q|manual-est.html|; 
-$noresave{$key} = "$nosave";
-
 $key = q/dependencies/;
 $external_labels{$key} = "$URL/" . q|manual-est.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig:hclustpange/;
+$key = q/fig:annotcluster/;
 $external_labels{$key} = "$URL/" . q|manual-est.html|; 
 $noresave{$key} = "$nosave";
 
@@ -74,19 +30,15 @@ $key = q/binaries/;
 $external_labels{$key} = "$URL/" . q|manual-est.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/options/;
+$key = q/fig:flow/;
 $external_labels{$key} = "$URL/" . q|manual-est.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/input/;
+$key = q/transcripts2cds/;
 $external_labels{$key} = "$URL/" . q|manual-est.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/FAQs/;
-$external_labels{$key} = "$URL/" . q|manual-est.html|; 
-$noresave{$key} = "$nosave";
-
-$key = q/tab:evcodes/;
+$key = q/default/;
 $external_labels{$key} = "$URL/" . q|manual-est.html|; 
 $noresave{$key} = "$nosave";
 
@@ -94,7 +46,55 @@ $key = q/fig:pant0/;
 $external_labels{$key} = "$URL/" . q|manual-est.html|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig:flow/;
+$key = q/tab:evcodes/;
+$external_labels{$key} = "$URL/" . q|manual-est.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/options/;
+$external_labels{$key} = "$URL/" . q|manual-est.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:histid/;
+$external_labels{$key} = "$URL/" . q|manual-est.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:pant2/;
+$external_labels{$key} = "$URL/" . q|manual-est.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/FAQs/;
+$external_labels{$key} = "$URL/" . q|manual-est.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:CNS/;
+$external_labels{$key} = "$URL/" . q|manual-est.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:ANImat/;
+$external_labels{$key} = "$URL/" . q|manual-est.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:snapshot/;
+$external_labels{$key} = "$URL/" . q|manual-est.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:ANIsample/;
+$external_labels{$key} = "$URL/" . q|manual-est.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/perlmods/;
+$external_labels{$key} = "$URL/" . q|manual-est.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:ESTcaveats/;
+$external_labels{$key} = "$URL/" . q|manual-est.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/input/;
+$external_labels{$key} = "$URL/" . q|manual-est.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:pangenomet/;
 $external_labels{$key} = "$URL/" . q|manual-est.html|; 
 $noresave{$key} = "$nosave";
 
@@ -105,35 +105,23 @@ $noresave{$key} = "$nosave";
 # labels from external_latex_labels array.
 
 
-$key = q/input/;
-$external_latex_labels{$key} = q|3.1|; 
-$noresave{$key} = "$nosave";
-
-$key = q/binaries/;
-$external_latex_labels{$key} = q|2.2|; 
-$noresave{$key} = "$nosave";
-
-$key = q/options/;
-$external_latex_labels{$key} = q|3.2|; 
+$key = q/default/;
+$external_latex_labels{$key} = q|4|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig:pant0/;
 $external_latex_labels{$key} = q|4.2|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig:flow/;
+$key = q/options/;
 $external_latex_labels{$key} = q|3.2|; 
-$noresave{$key} = "$nosave";
-
-$key = q/_/;
-$external_latex_labels{$key} = q|<|; 
 $noresave{$key} = "$nosave";
 
 $key = q/FAQs/;
 $external_latex_labels{$key} = q|5|; 
 $noresave{$key} = "$nosave";
 
-$key = q/tab:ANIsample/;
+$key = q/fig:pant2/;
 $external_latex_labels{$key} = q|4.2|; 
 $noresave{$key} = "$nosave";
 
@@ -141,31 +129,7 @@ $key = q/fig:CNS/;
 $external_latex_labels{$key} = q|13|; 
 $noresave{$key} = "$nosave";
 
-$key = q/tab:algs/;
-$external_latex_labels{$key} = q|1|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:pangenomet/;
-$external_latex_labels{$key} = q|4.2|; 
-$noresave{$key} = "$nosave";
-
-$key = q/dependencies/;
-$external_latex_labels{$key} = q|2.3|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:hclustpange/;
-$external_latex_labels{$key} = q|4.3|; 
-$noresave{$key} = "$nosave";
-
-$key = q/install/;
-$external_latex_labels{$key} = q|2|; 
-$noresave{$key} = "$nosave";
-
-$key = q/transcripts2cds/;
-$external_latex_labels{$key} = q|4.1|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:snapshot/;
+$key = q/fig:ANImat/;
 $external_latex_labels{$key} = q|4.2|; 
 $noresave{$key} = "$nosave";
 
@@ -173,28 +137,60 @@ $key = q/perlmods/;
 $external_latex_labels{$key} = q|2.1|; 
 $noresave{$key} = "$nosave";
 
+$key = q/fig:ESTcaveats/;
+$external_latex_labels{$key} = q|12|; 
+$noresave{$key} = "$nosave";
+
+$key = q/tab:ANIsample/;
+$external_latex_labels{$key} = q|4.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:pangenomet/;
+$external_latex_labels{$key} = q|4.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/input/;
+$external_latex_labels{$key} = q|3.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:snapshot/;
+$external_latex_labels{$key} = q|4.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:hclustpange/;
+$external_latex_labels{$key} = q|4.3|; 
+$noresave{$key} = "$nosave";
+
 $key = q/fig:RAMtimev2/;
 $external_latex_labels{$key} = q|11|; 
 $noresave{$key} = "$nosave";
 
-$key = q/default/;
-$external_latex_labels{$key} = q|4|; 
+$key = q/install/;
+$external_latex_labels{$key} = q|2|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig:ANImat/;
-$external_latex_labels{$key} = q|4.2|; 
+$key = q/tab:algs/;
+$external_latex_labels{$key} = q|1|; 
 $noresave{$key} = "$nosave";
 
-$key = q/fig:pant2/;
-$external_latex_labels{$key} = q|4.2|; 
+$key = q/dependencies/;
+$external_latex_labels{$key} = q|2.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:flow/;
+$external_latex_labels{$key} = q|3.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/transcripts2cds/;
+$external_latex_labels{$key} = q|4.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/binaries/;
+$external_latex_labels{$key} = q|2.2|; 
 $noresave{$key} = "$nosave";
 
 $key = q/fig:annotcluster/;
-$external_latex_labels{$key} = q|4.5|; 
-$noresave{$key} = "$nosave";
-
-$key = q/fig:ESTcaveats/;
-$external_latex_labels{$key} = q|12|; 
+$external_latex_labels{$key} = q|4.6|; 
 $noresave{$key} = "$nosave";
 
 1;
