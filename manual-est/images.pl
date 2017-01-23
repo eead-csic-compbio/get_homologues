4f9cd6c9a25b1219c591d415f3db70b1772714c6
eead-csic-compbio <compbio@eead.csic.es>
2017-01-23 12:53:06 +0100

    added trans2cds overall performance to table

--- manual-est/images.pl
+++ manual-est/images.pl
@@ -2,23 +2,11 @@
 # Associate images original text with physical files.
 
 
-$key = q/coveragege50%;MSF=1.6/;
+$key = q/geq40;MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="111" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img9.png"
- ALT="$ coverage \ge 50\%$">|; 
-
-$key = q/geq50%;MSF=1.6/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="49" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img4.png"
- ALT="$ \geq 50\%$">|; 
-
-$key = q/geq70%;MSF=1.6/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="49" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img3.png"
- ALT="$ \geq 70\%$">|; 
+ WIDTH="36" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$ \geq 40$">|; 
 
 $key = q/G;MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
@@ -26,23 +14,23 @@ $cached_env_img{$key} = q|<IMG
  SRC="|."$dir".q|img5.png"
  ALT="$ G$">|; 
 
-$key = q/.slashinstall.pl;MSF=1.6/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="77" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img7.png"
- ALT="$ ./install.pl$">|; 
-
 $key = q/occupancy>2;MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="101" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img8.png"
  ALT="$ occupancy &gt; 2$">|; 
 
-$key = q/geq40;MSF=1.6/;
+$key = q/geq70%;MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="36" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img1.png"
- ALT="$ \geq 40$">|; 
+ WIDTH="49" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$ \geq 70\%$">|; 
+
+$key = q/geq50%;MSF=1.6/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="49" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="$ \geq 50\%$">|; 
 
 $key = q/G^{2};MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
@@ -56,11 +44,23 @@ $cached_env_img{$key} = q|<IMG
  SRC="|."$dir".q|img10.png"
  ALT="$ identity \ge 70\%$">|; 
 
+$key = q/coveragege50%;MSF=1.6/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="111" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img9.png"
+ ALT="$ coverage \ge 50\%$">|; 
+
 $key = q/<70%;MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="49" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img2.png"
  ALT="$ &lt;70\%$">|; 
+
+$key = q/.slashinstall.pl;MSF=1.6/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="77" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img7.png"
+ ALT="$ ./install.pl$">|; 
 
 1;
 
