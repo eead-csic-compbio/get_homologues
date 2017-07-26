fe4ef8df73a2f841978b394e9a42873bc241ae85
eead-csic-compbio <compbio@eead.csic.es>
2017-07-26 09:03:24 +0200

    updated get_homologues.pl -g and checked this section in the manual

--- manual/images.pl
+++ manual/images.pl
@@ -2,29 +2,23 @@
 # Associate images original text with physical files.
 
 
-$key = q/G;MSF=1.6/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="16" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img1.png"
- ALT="$ G$">|; 
-
-$key = q/Evalue<max(Evalue);MSF=1.6/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="155" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img3.png"
- ALT="$ Evalue &lt; max(Evalue)$">|; 
-
 $key = q/G^{2};MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="22" HEIGHT="35" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img2.png"
  ALT="$ G^{2}$">|; 
 
-$key = q/identityge50%;MSF=1.6/;
+$key = q/coveragege50%&;MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="103" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img6.png"
- ALT="$ identity \ge 50\%$">|; 
+ WIDTH="111" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="$ coverage \ge 50\% &amp; $">|; 
+
+$key = q/G;MSF=1.6/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="16" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img1.png"
+ ALT="$ G$">|; 
 
 $key = q/coveragege20%;MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
@@ -32,11 +26,17 @@ $cached_env_img{$key} = q|<IMG
  SRC="|."$dir".q|img4.png"
  ALT="$ coverage \ge 20\%$">|; 
 
-$key = q/coveragege50%&;MSF=1.6/;
+$key = q/identityge50%;MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="111" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img5.png"
- ALT="$ coverage \ge 50\% &amp; $">|; 
+ WIDTH="103" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img6.png"
+ ALT="$ identity \ge 50\%$">|; 
+
+$key = q/Evalue<max(Evalue);MSF=1.6/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="155" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$ Evalue &lt; max(Evalue)$">|; 
 
 1;
 
