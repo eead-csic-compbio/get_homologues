1ebef5114d22916ab86d6a9c7e8846615bd67837
eead-csic-compbio <compbio@eead.csic.es>
2017-11-28 16:11:16 +0100

    fixed URLS of ForkManager and URI::Escape

--- manual/images.pl
+++ manual/images.pl
@@ -2,11 +2,17 @@
 # Associate images original text with physical files.
 
 
-$key = q/Evalue<max(Evalue);MSF=1.6/;
+$key = q/coveragege20%;MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="155" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img3.png"
- ALT="$ Evalue &lt; max(Evalue)$">|; 
+ WIDTH="111" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="$ coverage \ge 20\%$">|; 
+
+$key = q/G^{2};MSF=1.6/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="22" HEIGHT="35" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$ G^{2}$">|; 
 
 $key = q/G;MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
@@ -20,23 +26,17 @@ $cached_env_img{$key} = q|<IMG
  SRC="|."$dir".q|img5.png"
  ALT="$ coverage \ge 50\% &amp; $">|; 
 
-$key = q/coveragege20%;MSF=1.6/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="111" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img4.png"
- ALT="$ coverage \ge 20\%$">|; 
-
 $key = q/identityge50%;MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="103" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img6.png"
  ALT="$ identity \ge 50\%$">|; 
 
-$key = q/G^{2};MSF=1.6/;
+$key = q/Evalue<max(Evalue);MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
- WIDTH="22" HEIGHT="35" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img2.png"
- ALT="$ G^{2}$">|; 
+ WIDTH="155" HEIGHT="31" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img3.png"
+ ALT="$ Evalue &lt; max(Evalue)$">|; 
 
 1;
 
