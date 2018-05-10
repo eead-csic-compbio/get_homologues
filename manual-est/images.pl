e40f037b0f1c57d1ffbea6cd906325069842edb3
eead-csic-compbio <compbio@eead.csic.es>
2018-05-10 16:53:05 +0200

    added FAQ about get_phylo

--- manual-est/images.pl
+++ manual-est/images.pl
@@ -2,35 +2,11 @@
 # Associate images original text with physical files.
 
 
-$key = q/<70%;MSF=1.6/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="49" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img2.png"
- ALT="$ &lt;70\%$">|; 
-
 $key = q/identityge70%;MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="103" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img11.png"
  ALT="$ identity \ge 70\%$">|; 
-
-$key = q/coveragege50%;MSF=1.6/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="111" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img10.png"
- ALT="$ coverage \ge 50\%$">|; 
-
-$key = q/G;MSF=1.6/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="16" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img5.png"
- ALT="$ G$">|; 
-
-$key = q/geq30;MSF=1.6/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="36" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img9.png"
- ALT="$ \geq 30$">|; 
 
 $key = q/.slashinstall.pl;MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
@@ -38,11 +14,29 @@ $cached_env_img{$key} = q|<IMG
  SRC="|."$dir".q|img7.png"
  ALT="$ ./install.pl$">|; 
 
+$key = q/geq30;MSF=1.6/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="36" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img9.png"
+ ALT="$ \geq 30$">|; 
+
+$key = q/geq50%;MSF=1.6/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="49" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img4.png"
+ ALT="$ \geq 50\%$">|; 
+
 $key = q/geq40;MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="36" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img1.png"
  ALT="$ \geq 40$">|; 
+
+$key = q/G;MSF=1.6/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="16" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img5.png"
+ ALT="$ G$">|; 
 
 $key = q/G^{2};MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
@@ -50,23 +44,29 @@ $cached_env_img{$key} = q|<IMG
  SRC="|."$dir".q|img6.png"
  ALT="$ G^{2}$">|; 
 
-$key = q/geq70%;MSF=1.6/;
-$cached_env_img{$key} = q|<IMG
- WIDTH="49" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img3.png"
- ALT="$ \geq 70\%$">|; 
-
 $key = q/occupancy>2;MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="101" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
  SRC="|."$dir".q|img8.png"
  ALT="$ occupancy &gt; 2$">|; 
 
-$key = q/geq50%;MSF=1.6/;
+$key = q/geq70%;MSF=1.6/;
 $cached_env_img{$key} = q|<IMG
  WIDTH="49" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
- SRC="|."$dir".q|img4.png"
- ALT="$ \geq 50\%$">|; 
+ SRC="|."$dir".q|img3.png"
+ ALT="$ \geq 70\%$">|; 
+
+$key = q/<70%;MSF=1.6/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="49" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img2.png"
+ ALT="$ &lt;70\%$">|; 
+
+$key = q/coveragege50%;MSF=1.6/;
+$cached_env_img{$key} = q|<IMG
+ WIDTH="111" HEIGHT="29" ALIGN="MIDDLE" BORDER="0"
+ SRC="|."$dir".q|img10.png"
+ ALT="$ coverage \ge 50\%$">|; 
 
 1;
 
